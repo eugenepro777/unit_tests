95120b2be5e840e2d66561e1948c21d7a31d4949
Eugene_Pro <proskuryakov.e@list.ru>
2023-11-01 12:47:06 +0300

    create class Calculator

---
+++ src/main/java/homework1/task1/Calculator.java
@@ -0,0 +1,29 @@
+package homework1.task1;
+
+
+public class Calculator {
+
+
+    public double calculateDiscount(double purchaseAmount, double discountAmount) {
+        return purchaseAmount - purchaseAmount * (discountAmount / 100);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return (double) a / b;
+    }
+}
