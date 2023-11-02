c29fc2c4c7194d89dc48f1978729ed49d3329a24
Eugene_Pro <proskuryakov.e@list.ru>
2023-11-02 14:42:13 +0300

    solution task1

--- src/main/java/homework1/task1/Calculator.java
+++ src/main/java/homework1/task1/Calculator.java
@@ -4,8 +4,19 @@ package homework1.task1;
 public class Calculator {
 
 
-    public static double calculateDiscount(double purchaseAmount, double discountAmount) {
-        return purchaseAmount - purchaseAmount * (discountAmount / 100);
+    public static double calculateDiscount(double purchaseAmount, int discountAmount) {
+        double discountedAmount;
+
+        if (purchaseAmount >= 0) {
+            if (discountAmount >= 0 && discountAmount <= 70) {
+                discountedAmount = purchaseAmount - (purchaseAmount * discountAmount) / 100;
+            } else {
+                throw new ArithmeticException("Скидка должна быть в диапазоне от 0 до 70%");
+            }
+        } else {
+            throw new ArithmeticException("Сумма покупки не бывает отрицательной");
+        }
+        return discountedAmount;
     }
 
     public int add(int a, int b) {
--- src/test/java/homework1/task1/CalculatorTest.java
+++ src/test/java/homework1/task1/CalculatorTest.java
@@ -6,14 +6,20 @@ import static org.assertj.core.api.Assertions.*;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        
-        assertThat(Calculator.calculateDiscount(22.0, 7.0)).isEqualTo(20.46);
-        assertThat(Calculator.calculateDiscount(100.0, 10.0)).isEqualTo(90.0);
-        assertThat(Calculator.calculateDiscount(33.0, 0.0)).isEqualTo(33.0);
-        assertThatThrownBy(() -> Calculator.calculateDiscount(22, 7)).
-                isInstanceOf(ArithmeticException.class);
-        assertThatThrownBy(() -> Calculator.calculateDiscount(0, 0)).
-                isInstanceOf(ArithmeticException.class);
+
+//        проверяем утверждения, связанные с правильностью вычисления цены со скидкой
+        assertThat(Calculator.calculateDiscount(9000, 70)).isEqualTo(2700);
+        assertThat(Calculator.calculateDiscount(2200, 7)).isEqualTo(2046);
+        assertThat(Calculator.calculateDiscount(100, 10)).isEqualTo(90);
+        assertThat(Calculator.calculateDiscount(33, 0)).isEqualTo(33);
+//        проверяем утверждения, связанные с выбрасыванием ошибки
+        assertThatThrownBy(() -> Calculator.calculateDiscount(330, 71))
+                .isInstanceOf(ArithmeticException.class).hasMessage("Скидка в диапазоне от 0 до 70%");
+        assertThatThrownBy(() -> Calculator.calculateDiscount(1000, -50))
+                .isInstanceOf(ArithmeticException.class).hasMessage("Скидка в диапазоне от 0 до 70%");
+        assertThatThrownBy(() -> Calculator.calculateDiscount(-1000, 20))
+                .isInstanceOf(ArithmeticException.class).hasMessage("Сумма покупки не бывает отрицательной");
+
     }
 
 
