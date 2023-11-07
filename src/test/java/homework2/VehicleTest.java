e772b6383482b2f733ae163cf0fe80c0773ba67a
Eugene_Pro <proskuryakov.e@list.ru>
2023-11-07 19:51:52 +0300

    the main classes for our task have been created

---
+++ src/test/java/homework2/VehicleTest.java
@@ -0,0 +1,32 @@
+package homework2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testDriveCar() {
+    }
+
+    @Test
+    void testDriveMotorcycle() {
+    }
+
+    @Test
+    void park() {
+    }
+
+
+}
