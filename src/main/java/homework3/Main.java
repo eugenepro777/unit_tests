f655ae28caf1dd690f4e9efc114beabc1dc7fda1
Eugene_Pro <proskuryakov.e@list.ru>
2023-11-10 11:34:51 +0300

    the Main class was created with the isEvenOddNumber and isNumberInInterval methods for the first and second tasks

---
+++ src/main/java/homework3/Main.java
@@ -0,0 +1,22 @@
+package homework3;
+
+public class Main {
+
+    // Метод проверяет, является ли целое число записанное в переменную n, чётным (true) либо нечётным (false).
+    public boolean isEvenOddNumber(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Метод проверяет, попадает ли переданное число в интервал (25;100) и возвращает true или false
+    public boolean isNumberInInterval(int n) {
+        if (n > 25 && n < 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
